33c4e07b2e036477ed3b829a1b14ff6b33d125fd
Havar.Bathaee <h.bathaee@greenpack.de>
2020-03-10 15:25:17 +0100

    m

---
+++ p.py
@@ -0,0 +1,13 @@
+import sys
+
+print("start")
+print(sys.version)
+print(sys.executable)
+str = "havar"
+print(str.upper())
+print("havar")
+
+name = input("Your name?")
+print(name.lower())
+# print(greet('Corey'))
+
