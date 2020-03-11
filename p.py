72b439f4bca8b80210ce761ef0b2dc7a1a95cfdb
Havar <havarb@gmail.com>
2020-03-11 10:26:52 +0100

    m

--- p.py
+++ p.py
@@ -1,18 +1,24 @@
 import sys
 
 print("start")
-print(sys.version)
-print(sys.executable)
-str = "havar"
-print(str.upper())
-print("havar")
-
-
-
-
-
-
-
-name = input("Your name?")
-print(name.lower())
-# print(greet('Corey'))
+multipleline = '''line1
+line2
+line3'''
+print(multipleline)
+print(len(multipleline))
+print(multipleline[0])
+print(multipleline[6:].upper())
+l = multipleline.find('line2')
+st = 'find = '+ str(l)
+print(st)
+print(multipleline[l:])
+print('count = ' +str(multipleline.count('line')))
+# print("start")
+# print(sys.version)
+# print(sys.executable)
+# str = "havar"
+# print(str.upper())
+# print("havar")
+# name = input("Your name?")
+# print(name.lower())
+# # print(greet('Corey'))
